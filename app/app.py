305e4f0852b2449c54f2ca3f1314dd8da0d8dda4
Jayanth Venkataraman <jayanthvenkataraman@Jayanths-MacBook-Pro-2.local>
2016-05-13 08:11:53 +0530

    Modifying port to take ports corresponding to production and local

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 __author__ = 'jayanthvenkataraman'
 
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -10,4 +11,5 @@ def index():
     return "Heroku Flask application Successfully deployed"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
